cf30a8f64cf86254e03462caba26336fc0961097
Vasilii Rogin <vasilii@giosg.com>
2021-03-14 18:21:18 +0200

    Add test for timezone

--- src/transformers/openinghours.test.ts
+++ src/transformers/openinghours.test.ts
@@ -1,29 +1,36 @@
-import { isMainThread } from "worker_threads";
+import { DAYS_OF_WEEK } from "./defs";
 import { transformOpeningHours } from "./openinghours";
 
 describe("Opening hours transformer", () => {
-  it("Closed all days", () => {
-    expect(
-      transformOpeningHours(
-        {
-          monday: [],
-          tuesday: [],
-          wednesday: [],
-          thursday: [],
-          friday: [],
-          saturday: [],
-          sunday: [],
-        },
-        new Date("2021-03-14T16:04:37.974Z")
-      )
-    ).toEqual([
-      { dayLabel: "Monday", isToday: false, openHours: [] },
-      { dayLabel: "Tuesday", isToday: false, openHours: [] },
-      { dayLabel: "Wednesday", isToday: false, openHours: [] },
-      { dayLabel: "Thursday", isToday: false, openHours: [] },
-      { dayLabel: "Friday", isToday: false, openHours: [] },
-      { dayLabel: "Saturday", isToday: false, openHours: [] },
-      { dayLabel: "Sunday", isToday: true, openHours: [] },
-    ]);
-  });
+  it(`Timezone is ok`, () => expect(new Date().getTimezoneOffset()).toBe(-120));
+
+  for (const dayId of [0, 1, 2, 3, 4, 5, 6]) {
+    it(`Closed all days, today is ${DAYS_OF_WEEK[dayId]}`, () => {
+      const now = new Date(`2021-03-${14 + dayId}T16:04:37.974Z`);
+      expect(now.getDay()).toBe(dayId);
+
+      expect(
+        transformOpeningHours(
+          {
+            monday: [],
+            tuesday: [],
+            wednesday: [],
+            thursday: [],
+            friday: [],
+            saturday: [],
+            sunday: [],
+          },
+          now
+        )
+      ).toEqual([
+        { dayLabel: "Monday", isToday: dayId === 1, openHours: [] },
+        { dayLabel: "Tuesday", isToday: dayId === 2, openHours: [] },
+        { dayLabel: "Wednesday", isToday: dayId === 3, openHours: [] },
+        { dayLabel: "Thursday", isToday: dayId === 4, openHours: [] },
+        { dayLabel: "Friday", isToday: dayId === 5, openHours: [] },
+        { dayLabel: "Saturday", isToday: dayId === 6, openHours: [] },
+        { dayLabel: "Sunday", isToday: dayId === 7, openHours: [] },
+      ]);
+    });
+  }
 });
