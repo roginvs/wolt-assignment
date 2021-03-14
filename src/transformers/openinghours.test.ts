8575704dde86964f9137fed8d9af8ca205f0f511
Vasilii Rogin <vasilii@giosg.com>
2021-03-14 19:13:17 +0200

    Add check for ascending time

--- src/transformers/openinghours.test.ts
+++ src/transformers/openinghours.test.ts
@@ -1,6 +1,7 @@
 import { DAYS_OF_WEEK } from "./defs";
 import {
   appendTailFromNextDay,
+  checkTimeIsAsc,
   cutTailFromPrevDay,
   transformOpeningHours,
 } from "./openinghours";
@@ -155,33 +156,52 @@ describe("Opening hours transformer", () => {
       ]));
   });
 
-  for (const dayId of [0, 1, 2, 3, 4, 5, 6]) {
-    it(`Closed all days, today is ${DAYS_OF_WEEK[dayId]}`, () => {
-      const now = new Date(`2021-03-${14 + dayId}T16:04:37.974Z`);
-      expect(now.getDay()).toBe(dayId);
+  describe("Helper checkTimeIsAsc", () => {
+    expect(() => checkTimeIsAsc([])).not.toThrow();
+    expect(() => checkTimeIsAsc([{ type: "open", value: 1 }])).not.toThrow();
+    expect(() =>
+      checkTimeIsAsc([
+        { type: "open", value: 1 },
+        { type: "open", value: 2 },
+      ])
+    ).not.toThrow();
+    expect(() =>
+      checkTimeIsAsc([
+        { type: "open", value: 1 },
+        { type: "open", value: 1 },
+      ])
+    ).toThrow("Expecting time rangers to ascend");
+  });
 
-      expect(
-        transformOpeningHours(
-          {
-            monday: [],
-            tuesday: [],
-            wednesday: [],
-            thursday: [],
-            friday: [],
-            saturday: [],
-            sunday: [],
-          },
-          now
-        )
-      ).toEqual([
-        { dayLabel: "Monday", isToday: dayId === 1, openHours: [] },
-        { dayLabel: "Tuesday", isToday: dayId === 2, openHours: [] },
-        { dayLabel: "Wednesday", isToday: dayId === 3, openHours: [] },
-        { dayLabel: "Thursday", isToday: dayId === 4, openHours: [] },
-        { dayLabel: "Friday", isToday: dayId === 5, openHours: [] },
-        { dayLabel: "Saturday", isToday: dayId === 6, openHours: [] },
-        { dayLabel: "Sunday", isToday: dayId === 0, openHours: [] },
-      ]);
-    });
-  }
+  describe("Closed all days", () => {
+    for (const dayId of [0, 1, 2, 3, 4, 5, 6]) {
+      it(`Today is ${DAYS_OF_WEEK[dayId]}`, () => {
+        const now = new Date(`2021-03-${14 + dayId}T16:04:37.974Z`);
+        expect(now.getDay()).toBe(dayId);
+
+        expect(
+          transformOpeningHours(
+            {
+              monday: [],
+              tuesday: [],
+              wednesday: [],
+              thursday: [],
+              friday: [],
+              saturday: [],
+              sunday: [],
+            },
+            now
+          )
+        ).toEqual([
+          { dayLabel: "Monday", isToday: dayId === 1, openHours: [] },
+          { dayLabel: "Tuesday", isToday: dayId === 2, openHours: [] },
+          { dayLabel: "Wednesday", isToday: dayId === 3, openHours: [] },
+          { dayLabel: "Thursday", isToday: dayId === 4, openHours: [] },
+          { dayLabel: "Friday", isToday: dayId === 5, openHours: [] },
+          { dayLabel: "Saturday", isToday: dayId === 6, openHours: [] },
+          { dayLabel: "Sunday", isToday: dayId === 0, openHours: [] },
+        ]);
+      });
+    }
+  });
 });
--- src/transformers/openinghours.ts
+++ src/transformers/openinghours.ts
@@ -16,6 +16,16 @@ const DAY_LABELS: Record<DayOfWeek, string> = {
   sunday: "Sunday",
 };
 
+export class InputError extends Error {
+  // Nothing here
+  // TODO: Check error class instance in tests
+}
+export class InternalError extends Error {
+  public constructor() {
+    super("Internal error");
+  }
+}
+
 export function cutTailFromPrevDay(
   thisDay: DeepImmutable<OpeningHoursDayItem[]>,
   prevDay: DeepImmutable<OpeningHoursDayItem[]>
@@ -25,7 +35,7 @@ export function cutTailFromPrevDay(
     const isPrevDayHaveCloseTail =
       prevDay.length > 0 && prevDay[prevDay.length - 1]?.type === "open";
     if (!isPrevDayHaveCloseTail) {
-      throw new Error(`Previous day must contain "open" as last item`);
+      throw new InputError(`Previous day must contain "open" as last item`);
     }
     return thisDay.slice(1);
   } else {
@@ -46,11 +56,26 @@ export function appendTailFromNextDay(
         ? nextDay[0]
         : undefined;
     if (!nextDayStartsWith) {
-      throw new Error(`Next day must contain "close" as first item`);
+      throw new InputError(`Next day must contain "close" as first item`);
     }
     return [...thisDay, nextDayStartsWith];
   } else {
     return thisDay;
+  }
+}
+
+export function checkTimeIsAsc(
+  thisDay: DeepImmutable<OpeningHoursDayItem[]>
+): void {
+  for (let i = 0; i < thisDay.length - 1; i++) {
+    const thisItem = thisDay[i];
+    const nextItem = thisDay[i + 1];
+    if (!thisItem || !nextItem) {
+      throw new InternalError();
+    }
+    if (thisItem.value >= nextItem.value) {
+      throw new InputError("Expecting time rangers to ascend");
+    }
   }
 }
 
@@ -63,25 +88,24 @@ export function transformOpeningHours(
   for (const [dayName, dayIndex] of DAYS_OF_WEEK.map(
     (dayOfWeek, index) => [dayOfWeek, index] as const
   )) {
-    const isToday = now.getDay() === dayIndex;
-    const nextDayIndex = (dayIndex + 1) % 7;
-    const prevDayIndex = (dayIndex + 6) % 7;
-
     const thisDay = src[dayName];
+    checkTimeIsAsc(thisDay);
+
+    const prevDayIndex = (dayIndex + 6) % 7;
     const prevDayName = DAYS_OF_WEEK[prevDayIndex];
     if (!prevDayName) {
-      throw new Error("Internal error");
+      throw new InternalError();
     }
     const prevDay = src[prevDayName];
 
+    const nextDayIndex = (dayIndex + 1) % 7;
     const nextDayName = DAYS_OF_WEEK[nextDayIndex];
     if (!nextDayName) {
-      throw new Error("Internal error");
+      throw new InternalError();
     }
     const nextDay = src[nextDayName];
 
-    //const thisDa
-
+    const isToday = now.getDay() === dayIndex;
     result.push({
       dayLabel: DAY_LABELS[dayName],
       isToday: isToday,
@@ -92,7 +116,7 @@ export function transformOpeningHours(
   // Push Sunday to the end
   const sunday = result.shift();
   if (!sunday) {
-    throw new Error("Internal error");
+    throw new InternalError();
   }
   result.push(sunday);
 
