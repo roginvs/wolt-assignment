2be192c01d22071eea3788b2ec2d645ea6294dcf
Vasilii Rogin <vasilii@giosg.com>
2021-03-14 18:53:48 +0200

    Add helper functions

---
+++ src/deepImmutable.ts
@@ -0,0 +1,26 @@
+type Primitive = undefined | null | boolean | string | number | Function;
+
+type Immutable<T> = T extends Primitive
+  ? T
+  : T extends Array<infer U>
+  ? ReadonlyArray<U>
+  : T extends Map<infer K, infer V>
+  ? ReadonlyMap<K, V>
+  : Readonly<T>;
+
+export type DeepImmutable<T> = T extends Primitive
+  ? T
+  : T extends Array<infer U>
+  ? DeepImmutableArray<U>
+  : T extends Map<infer K, infer V>
+  ? DeepImmutableMap<K, V>
+  : DeepImmutableObject<T>;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DeepImmutableArray<T> extends ReadonlyArray<DeepImmutable<T>> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DeepImmutableMap<K, V>
+  extends ReadonlyMap<DeepImmutable<K>, DeepImmutable<V>> {}
+type DeepImmutableObject<T> = { readonly [K in keyof T]: DeepImmutable<T[K]> };
+
+export type UnDeepImmutable<T> = T extends DeepImmutable<infer U> ? U : never;
--- src/transformers/openinghours.ts
+++ src/transformers/openinghours.ts
@@ -1,7 +1,9 @@
+import { DeepImmutable } from "../deepImmutable";
 import {
   DayOfWeek,
   DAYS_OF_WEEK,
   OpeningHours,
+  OpeningHoursDayItem,
   ViewOpeningHours,
 } from "./defs";
 const DAY_LABELS: Record<DayOfWeek, string> = {
@@ -14,16 +16,71 @@ const DAY_LABELS: Record<DayOfWeek, string> = {
   sunday: "Sunday",
 };
 
+export function cutTailFromPrevDay(
+  thisDay: DeepImmutable<OpeningHoursDayItem[]>,
+  prevDay: DeepImmutable<OpeningHoursDayItem[]>
+): DeepImmutable<OpeningHoursDayItem[]> {
+  const isHaveTailFromLastDay = thisDay[0]?.type === "close";
+  if (isHaveTailFromLastDay) {
+    const isPrevDayHaveCloseTail =
+      prevDay.length > 0 && prevDay[prevDay.length - 1]?.type === "open";
+    if (!isPrevDayHaveCloseTail) {
+      throw new Error(`Previous day must contain "open" as last item`);
+    }
+    return thisDay.slice(1);
+  } else {
+    return thisDay;
+  }
+}
+
+export function appendTailFromNextDay(
+  thisDay: DeepImmutable<OpeningHoursDayItem[]>,
+  nextDay: DeepImmutable<OpeningHoursDayItem[]>
+): DeepImmutable<OpeningHoursDayItem[]> {
+  const isExpectingTailOnNextDay =
+    thisDay.length > 0 && thisDay[thisDay.length - 1]?.type === "open";
+
+  if (isExpectingTailOnNextDay) {
+    const nextDayStartsWith =
+      nextDay.length > 0 && nextDay[0]?.type === "close"
+        ? nextDay[0]
+        : undefined;
+    if (!nextDayStartsWith) {
+      throw new Error(`Next day must contain "close" as first item`);
+    }
+    return [...thisDay, nextDayStartsWith];
+  } else {
+    return thisDay;
+  }
+}
+
 export function transformOpeningHours(
-  src: OpeningHours,
+  src: DeepImmutable<OpeningHours>,
   now: Date = new Date()
-): ViewOpeningHours {
+): DeepImmutable<ViewOpeningHours> {
   const result: ViewOpeningHours = [];
 
   for (const [dayName, dayIndex] of DAYS_OF_WEEK.map(
     (dayOfWeek, index) => [dayOfWeek, index] as const
   )) {
     const isToday = now.getDay() === dayIndex;
+    const nextDayIndex = (dayIndex + 1) % 7;
+    const prevDayIndex = (dayIndex + 6) % 7;
+
+    const thisDay = src[dayName];
+    const prevDayName = DAYS_OF_WEEK[prevDayIndex];
+    if (!prevDayName) {
+      throw new Error("Internal error");
+    }
+    const prevDay = src[prevDayName];
+
+    const nextDayName = DAYS_OF_WEEK[nextDayIndex];
+    if (!nextDayName) {
+      throw new Error("Internal error");
+    }
+    const nextDay = src[nextDayName];
+
+    //const thisDa
 
     result.push({
       dayLabel: DAY_LABELS[dayName],
