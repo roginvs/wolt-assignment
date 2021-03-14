270546d5e94e3c38b1f78bbd3d1766550d7ed54c
Vasilii Rogin <vasilii@giosg.com>
2021-03-14 20:45:00 +0200

    Add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
+import { App } from "./view/App";
 
 const root = document.createElement("div");
 document.body.appendChild(root);
 
-ReactDOM.render(<div>Lol</div>, root);
+ReactDOM.render(<App />, root);
---
+++ src/view/App.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@import "https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap";
+
+* {
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'Roboto', sans-serif;
+    text-rendering: optimizelegibility;
+    height: 100%;
+    width: 100%;
+}
+
+html {
+    height: 100%
+}
+
+body > div {
+    height: 100%
+}
+`;
+
+const Container = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+});
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Container>
+        <div>asdasd</div>
+      </Container>
+    </>
+  );
+}
