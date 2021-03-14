f3fb245d5e711c4c475c8523a5e3281efd3eb9d0
Vasilii Rogin <vasilii@giosg.com>
2021-03-14 18:57:26 +0200

    Add test for transformer helper

--- src/transformers/openinghours.test.ts
+++ src/transformers/openinghours.test.ts
@@ -1,11 +1,91 @@
 import { DAYS_OF_WEEK } from "./defs";
-import { transformOpeningHours } from "./openinghours";
+import { cutTailFromPrevDay, transformOpeningHours } from "./openinghours";
 
 describe("Opening hours transformer", () => {
   it(`Timezone is ok`, () =>
     expect(new Date(`2021-03-14T16:04:37.974Z`).getTimezoneOffset()).toBe(
       -120
     ));
+
+  describe("Helper function cutTailFromPrevDay", () => {
+    it(`Empty items`, () => expect(cutTailFromPrevDay([], [])).toEqual([]));
+    it(`No tail from previous day`, () =>
+      expect(
+        cutTailFromPrevDay(
+          [
+            {
+              type: "open",
+              value: 100,
+            },
+            {
+              type: "close",
+              value: 200,
+            },
+          ],
+          []
+        )
+      ).toEqual([
+        {
+          type: "open",
+          value: 100,
+        },
+        {
+          type: "close",
+          value: 200,
+        },
+      ]));
+
+    it(`Throws if no tail from previous day`, () =>
+      expect(() =>
+        cutTailFromPrevDay(
+          [
+            {
+              type: "close",
+              value: 50,
+            },
+            {
+              type: "open",
+              value: 200,
+            },
+            {
+              type: "close",
+              value: 300,
+            },
+          ],
+          []
+        )
+      ).toThrow('Previous day must contain "open" as last item'));
+
+    it(`Have tail from previous day`, () =>
+      expect(
+        cutTailFromPrevDay(
+          [
+            {
+              type: "close",
+              value: 50,
+            },
+            {
+              type: "open",
+              value: 200,
+            },
+            {
+              type: "close",
+              value: 300,
+            },
+          ],
+          [{ type: "open", value: 1000 }]
+        )
+      ).toEqual([
+        {
+          type: "open",
+          value: 200,
+        },
+        {
+          type: "close",
+          value: 300,
+        },
+      ]));
+  });
 
   for (const dayId of [0, 1, 2, 3, 4, 5, 6]) {
     it(`Closed all days, today is ${DAYS_OF_WEEK[dayId]}`, () => {
